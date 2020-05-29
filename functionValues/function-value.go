5eb94e6e117a32de0ed435aacbcb63e42e12681f
akihisa1210 <19635263+akihisa1210@users.noreply.github.com>
2020-05-30 02:26:39 +0900

    Function Values

---
+++ functionValues/function-value.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func compute(fn func(float64, float64) float64) float64 {
+	return fn(3, 4)
+}
+
+func functionValues() {
+
+	hypot := func(x, y float64) float64 {
+		return math.Sqrt(x*x + y*y)
+	}
+	fmt.Println(hypot(5, 12))
+
+	fmt.Println(compute(hypot))
+	fmt.Println(compute(math.Pow))
+}
+
+// adder returns function (func(int) int).
+func adder() func(int) int {
+	sum := 0
+	// The following function is returned by adder()
+	return func(x int) int {
+		sum += x
+		return sum
+	}
+}
+
+func functionClosures() {
+	pos, neg := adder(), adder()
+	for i := 0; i < 10; i++ {
+		fmt.Println(
+			pos(i),
+			neg(-2*i),
+		)
+	}
+}
+
+func main() {
+	functionValues()
+	functionClosures()
+}
